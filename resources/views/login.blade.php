9bef9fbd72a0b967bd7f0471d0be8576719b8b79
rigo mac <mbandrigo@gmail.com>
2024-06-10 10:51:13 +0800

    https

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -65,8 +65,8 @@
 </head>
 
 <body class="h-100">
-    <!-- <div id="root" class="h-100"> -->
-    <div class="h-100">
+    <div id="root" class="h-100">
+        <!-- <div class="h-100"> -->
         <!-- Background Start -->
         <div class="fixed-background"></div>
         <!-- Background End -->
@@ -96,7 +96,7 @@
                 <!-- Left Side End -->
 
                 <!-- Right Side Start -->
-                <div class="col-12 col-lg-auto h-100 pb-4 px-4 pt-0 p-lg-0">
+                <div class="col-12 col-lg-auto h-100 pb-4 px-4 pt-0 p-lg-0" style="background-color:white">
                     <div class="sw-lg-70 min-h-100 bg-foreground d-flex justify-content-center align-items-center shadow-deep py-5 full-page-content-right-border">
                         <div class="sw-lg-50 px-5">
                             <div class="sh-11">
